3ac08b62b761c22ff714dc98567e9e42b9dc7629
Kappaskowi <lawrence_10@hotmail.de>
2017-07-08 20:24:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,18 @@
 //bot.registry.registerCommandsIn(__dirname + "/commands");
 
 //bot.login(process.env.BOT_TOKEN);
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+const fs = require("fs");
+bot.registry.registerGroup('random','Random');
 
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+
+var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
+bot.login(process.env.BOT_TOKEN);
+/*
 // Requires
 const Discord = require('discord.js')
 
@@ -19,7 +30,7 @@ client.on('ready', () => {
 })
 
 client.login(process.env.BOT_TOKEN)
-/*const Discord = require("discord.js");
+const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
 client.login("");
